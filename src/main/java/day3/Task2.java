c51cff287ad920c363d73d68f1fa055ff2c13ba0
AlikaKovtun <kovtun.alika@gmail.com>
2020-12-26 14:36:15 +0200

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите делимое: ");
+            double divident = scan.nextDouble();
+            System.out.print("Введите делитель: ");
+            double divider = scan.nextDouble();
+            if (divider == 0) return;
+            System.out.println("Результат деления: " + (divident / divider));
+
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Введите делимое: ");
+            double divident = scan.nextDouble();
+            System.out.print("Введите делитель: ");
+            double divider = scan.nextDouble();
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println("Результат деления: " + (divident / divider));
+        }
 
     }
 }
