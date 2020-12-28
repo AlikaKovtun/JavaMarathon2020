adcdd3abbd07eaa9cc351f0fe86712ceb41cff65
AlikaKovtun <kovtun.alika@gmail.com>
2020-12-28 22:49:49 +0200

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,14 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car astonMartin = new Car();
+        astonMartin.setColor("Black");
+        astonMartin.setModel("Aston Martin Vanquish");
+        astonMartin.setYear(2020);
+
+        System.out.println("The best car ever is " + astonMartin.getColor() + " " + astonMartin.getModel()
+                + " " + astonMartin.getYear() + " year");
+
+
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,9 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike moto = new Motorbike(2019, "Red", "GB12");
+
+        System.out.println("Motorbike: " + moto.getModel() + "\nColor: " + moto.getColor() + "\nYear: " + moto.getYear());
+
     }
 }
