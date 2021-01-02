0f46bfe0ea53733a5744e5b5d5582be2cab2b85c
AlikaKovtun <kovtun.alika@gmail.com>
2021-01-02 16:41:39 +0200

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,26 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startString = System.currentTimeMillis();
+        String s1 = "";
+        for (int i = 0; i <= 20000; i++) {
+            s1 += i + " ";
+
+        }
+        System.out.println(s1);
+        long finishString = System.currentTimeMillis();
+
+        long startStringBuilder = System.currentTimeMillis();
+        StringBuilder s2 = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            s2.append(i).append(" ");
+        }
+        System.out.println(s2.toString());
+        long finishStringBuilder = System.currentTimeMillis();
+
+        System.out.println("Время затраченное с использованием String: " + (finishString - startString));
+        System.out.println("Время затраченное с использованием StringBuilder: " + (finishStringBuilder - startStringBuilder));
+
+
     }
 }
