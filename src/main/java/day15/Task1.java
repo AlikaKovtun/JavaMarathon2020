1067ba16c3c6bdf4e2941da6f788b2368f54ef76
AlikaKovtun <kovtun.alika@gmail.com>
2021-01-09 13:44:58 +0200

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,38 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> absentShoes = findAbsentShoes(new File("src/main/resources/shoes.csv"));
+        createTXTfile(new File("src/main/resources/missing_shoes.txt"), absentShoes);
+
+
+    }
+
+    public static List<String> findAbsentShoes(File file) throws FileNotFoundException {
+        List<String> shoes = new ArrayList<>();
+        Scanner scan = new Scanner(file);
+        while (scan.hasNextLine()) {
+            String pair = scan.nextLine();
+            String[] info = pair.split(";");
+            if (Integer.parseInt(info[2]) == 0) {
+                shoes.add(info[0] + ", " + info[1] + ", " + info[2]);
+            }
+        }
+        return shoes;
+    }
+
+    public static void createTXTfile(File file, List<String> list) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(file);
+        for (String s : list) {
+            pw.print(s + "\n");
+        }
+        pw.close();
     }
 }
